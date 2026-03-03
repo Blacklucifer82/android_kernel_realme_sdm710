db55a428a8be554cd35adb4480e82908454af3d8
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-03-03 15:11:35 +0800

    fs: susfs: Optimize SUS_MOUNT and other optimizations - Fix several race issues by using proper locks:   down_read(&namespace_sem); // needed when manipulating mnt_namespace   lock_ns_list(mnt_ns); // needed when traversing mnt_ns->list   lock_mount_hash(); // needed when modifying mount - In the end, the mnt_id is not guaranteed to allocated sequentially for a namespace, this happens even on newer stock locked devices, and mnt_group_id gap is the only one we really need to care for, real world apps should not rely on mnt_id gaps for root detections. - Re-use the mnt_id_ida to assign fake mnt_id with the minimum mnt_id set to DEFAULT_UNSHARE_KSU_MNT_ID (default 400000) for mounts unshared by ksu process and DEFAULT_KSU_MNT_ID (default 500000) for mounts created or single cloned by ksu process, so we no longer need to hook mnt_free_id().
    - For global susfs bool variables, use WRITE_ONCE() and READ_ONCE() to ensure the atomicity.
    - For global susfs char variables, use seqlock for frequently read and rarely write, but need no irq.
    - Add susfs_is_current_proc_umounted_app(), used to check if current proc is marked umounted and current uid >= 10000. Most of the time we should use this now to apply on app uid only, and use susfs_is_current_proc_umounted() only for sucampat check.
    - Check for null of all inode->i_state.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -109,7 +109,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_non_su_procs &&
+	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
 			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 			!susfs_is_current_ksu_domain())
 	{
@@ -154,7 +154,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_non_su_procs &&
+	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
 			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 			!susfs_is_current_ksu_domain())
 	{
@@ -227,7 +227,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_non_su_procs &&
+	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
 			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 			!susfs_is_current_ksu_domain())
 	{
